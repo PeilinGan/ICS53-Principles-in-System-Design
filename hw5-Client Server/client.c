d8f2dca145fd7ef133b1c998c7c1840e9321c213
PeilinGan <pgan@uci.edu>
2020-03-01 13:02:41 -0800

    client

--- hw5-Client Server/client.c
+++ hw5-Client Server/client.c
@@ -1,16 +1,43 @@
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <string.h> 
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+int MAXLINE = 200;
 
-int main(int argc) {
-    int server_fd;
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    { 
-        perror("socket failed"); 
-        exit(EXIT_FAILURE); 
-    } 
+int main(int argc, char *argv[])
+{
+    int client_socket;
+    struct sockaddr_in server_address;
+    memset(&server_address, '0', sizeof(server_address));
+
+    // int server_address_len = sizeof(server_address);
+
+    int port_num = atoi(argv[2]);
+
+    //! <0 ?
+    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port_num);
+    if (inet_pton(AF_INET, argv[1], & server_address.sin_addr) <= 0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    // server_address.sin_addr.s_addr = INADDR_ANY;
+    if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        exit(EXIT_FAILURE);
+    }
+
     return 0;
 }
